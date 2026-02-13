88a6aaa69543703c4a8756301598797895e19058
OP-jerbe <j.erbe@oregon-physics.com>
2026-02-13 14:17:58 -0800

    add comport to Stage argument and make open_connection more robust

--- src/model/stage.py
+++ src/model/stage.py
@@ -1,16 +1,38 @@
-import serial
+from threading import Lock
+from typing import Optional
 
-from src.helpers.helpers import load_ini
+import serial
 
 
 class Stage:
-    def __init__(self) -> None:
-        self.comport = self._get_comport()
+    def __init__(self, com_port: Optional[str] = None) -> None:
+        self._lock = Lock()
+        self.com_port = com_port
+        self.serial_port: Optional[serial.Serial] = None
 
-    @staticmethod
-    def _get_comport() -> str:
-        config_data = load_ini()
-        return config_data.get('COMPORT', 'port')
+        if self.com_port:
+            self.open_connection(self.com_port)
 
-    def connect(self, port) -> None:
-        serial.Serial(port)
+    def open_connection(
+        self, port: str, baudrate: int = 9600, timeout: float = 1.0
+    ) -> None:
+        """
+        Establishes a serial connection to the instrument at the specified COM port.
+
+        Args:
+            port (str): The COM port where the stage is connected (e.g., 'COM3' or '/dev/ttyUSB0').
+                The port name is automatically converted to uppercase.
+            baudrate (int): The serial communication baud rate in bits per second. Defaults to 9600.
+            timeout (float): The read and write timeout in seconds. Defaults to 1.0.
+        """
+        try:
+            self.serial_port = serial.Serial(
+                port=port.upper(),
+                baudrate=baudrate,
+                timeout=timeout,
+                write_timeout=timeout,
+            )
+
+        except Exception as e:
+            print(f'Failed to make a serial connection to {port}.\n\n{str(e)}')
+            self.serial_port = None
